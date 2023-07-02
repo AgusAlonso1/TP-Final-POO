54013a5faa7d0db7742399f74566d8b785616a24
AgusAlonso1 <112195368+AgusAlonso1@users.noreply.github.com>
2023-07-02 15:09:22 -0300

    Added provided source code with modifications.

---
+++ SourceCode/src/frontend/MainFrame.java
@@ -0,0 +1,13 @@
+package frontend;
+
+import backend.CanvasState;
+import javafx.scene.layout.VBox;
+
+public class MainFrame extends VBox {
+
+    public MainFrame(CanvasState canvasState) {
+        StatusPane statusPane = new StatusPane();
+        getChildren().addAll(new AppMenuBar(), new PaintPane(canvasState, statusPane),statusPane);
+    }
+
+}
