f3b5551c1ad44fce867d1e1cefcfe2269630b695
AgusAlonso1 <112195368+AgusAlonso1@users.noreply.github.com>
2023-07-06 14:23:23 -0300

    Continue implementation of figure tags front-end.

--- SourceCode/src/frontend/TagsBar.java
+++ SourceCode/src/frontend/TagsBar.java
@@ -1,5 +1,6 @@
 package frontend;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 
@@ -8,9 +9,13 @@ public class TagsBar extends HBox {
     private RadioButton allTagsButton, specificTagsButton;
     private TextField specificTags;
     public TagsBar() {
+        setStyle("-fx-background-color: #999");
+        setAlignment(Pos.CENTER);
         Label label = new Label("Mostrar Etiquetas:");
         this.allTagsButton = new RadioButton("Todas");
         this.specificTagsButton = new RadioButton("Solo");
+        ToggleGroup toggleGroup = new ToggleGroup();
+        allTagsButton.setToggleGroup(toggleGroup);
         this.specificTags = new TextField();
         this.getChildren().addAll(label,allTagsButton,specificTagsButton,specificTags);
         setSpacing(SPACING);
